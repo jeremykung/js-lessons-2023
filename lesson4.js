d7ec63452d7bba2e2dae98ecdee0bff528c6acae
jeremykung <jeremykung@gmail.com>
2023-02-09 10:35:21 +0800

    updated functions lesson4 with parameters

---
+++ lesson4.js
@@ -0,0 +1,54 @@
+/*
+    Functions
+    1. Defining
+    2. Calling
+    3. Parameters / Arguments
+    4. Callback Functions
+
+*/
+
+// Define
+
+function alleyOop() {
+    document.write('Jennifer got the ball')
+    document.write('Markus made a great aerial pass')
+    document.write('Jennifer dunked over Jeff')
+    console.log(true)
+}
+
+// Call
+
+alleyOop()
+alleyOop()
+
+// Practice
+// Define 2 more functions
+// Call them multiple times
+
+
+
+// Define with Parameters
+function reject(player) {
+    document.write(player + ' was rejected!')
+    console.log(false)
+}
+
+function add3(num) {
+    const theNum = parseInt(num)
+    const answer = theNum + 3
+    console.log('answer is ' + answer)
+}
+
+function multiply(num1, num2) {
+    const answer = num1 * num2
+    console.log(`${num1} x ${num2} = ${answer}`)
+}
+
+// Call with Arguments
+reject('Jeff')
+add3(1)
+multiply(5, 10)
+
+// Practice
+// Define and call a function that adds 50, multiplies by 2, then divides by 3
+// Define and call a function that says hello to 2 different people
