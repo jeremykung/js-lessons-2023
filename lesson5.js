77281e3e69d59cde3d8497f37cf3a097c1ebab85
jeremykung <jeremykung@gmail.com>
2023-02-16 10:36:24 +0800

    updated lesson 5 for clearInterval

--- lesson5.js
+++ lesson5.js
@@ -3,7 +3,32 @@
     2.DOM
         - querySelector
         - elementById
+    3.Funciton Variables
+
 */
+let sayHiMany
+let num = 0
+let numInterval
+
+function start() {
+    numInterval = setInterval(addNum, 1000)
+}
+
+function stop() {
+    clearInterval(numInterval)
+}
+
+function reset() {
+    // 1. make num 0
+    num = 0
+    // 2. stop interval
+    clearInterval(numInterval)
+}
+
+function addNum() {
+    num++
+    console.log(num)
+}
 
 function sayHi() {
     console.log('hi')
