6bec34c041fc7c3a12044a794242789b63300c83
jeremykung <jeremykung@gmail.com>
2023-02-27 10:26:25 +0800

    added lesson 6

---
+++ lesson6.js
@@ -0,0 +1,66 @@
+/*
+
+1. Arrays
+    - What are they?
+    - How do we define them?
+    - How do we access them?
+    - Elements and Indexes
+    - How do we manipulate them?
+    - push() and pop()
+*/
+
+// Array
+// list of things
+
+const student1 = "Markus"
+const student2 = "Jennifer"
+const student3 = "Evan"
+const student4 = "Skyler"
+
+// Define an array
+const students = ["Markus", "Jennifer", "Evan"]
+
+console.log(students[2])
+
+// Try Yourself
+// Create an array of numbers
+// console.log() the 4th number in the array
+
+const numbers = [3, 4, 6, 9, 2]
+// console.log(numbers)
+
+// Add things to an array
+numbers.push(7)
+// numbers.push(8)
+
+// Remove things from an array
+numbers.pop()
+
+// console.log(numbers)
+
+// Arrays can store anything of any type
+const randomThings = [32, "chocolate", true]
+
+
+function pop() {
+    // remove last thing on array
+    // return what you removed
+}
+
+
+// Function returns
+function sayHi() {
+    console.log('hi')
+    return 5
+}
+
+// Create an array of your top 3 foods
+// Add 2 more foods to the end of the array
+// Show the 4th item of the array in your HTML
+
+
+// Create an array of your worst enemies
+// Show them all in your HTML
+// Then delete the last enemy
+// Show the deleted enemy in your HTML
+//      - ex. "Enemy1 was Destroyed"
