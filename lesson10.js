523559ce5b28f8d03eada8ff916f3c5cd81fec48
jeremykung <jeremykung@gmail.com>
2023-04-06 09:58:21 +0800

    updated notes

--- lesson10.js
+++ lesson10.js
@@ -1,24 +1,28 @@
 /*
 
-1. Reading form values
-2. Local Storage
+1. Reading data form values
+2. Adding data objects to an array
 
 */
 
 let students = []
 
-function handleClick() {
+function addStudent() {
+
     // 1. read the value from the input
     const nameData = document.querySelector("#name").value
     const gradeData = document.querySelector("#grade").value
-    // 2. console.log the value
+
+    // 2. console.log the value to confirm we have it
     console.log(nameData, gradeData)
+
     // 3. create object with form data
     const student = {
         name: nameData,
         grade: gradeData
     }
-    // 4. add that data to my array
+
+    // 4. add object to array
     students.push(student)
 }
 
